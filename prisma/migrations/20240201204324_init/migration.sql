30fb8ee93b7dd0de333dff2e98cc5afa0a467c17
mohit <mohit@mohits-MacBook-Pro.local>
2024-02-12 14:22:19 +0530

    Add existing project files to Git

---
+++ prisma/migrations/20240201204324_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Items" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "length" INTEGER NOT NULL,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "stackable" BOOLEAN NOT NULL DEFAULT false,
+    "tiltable" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Items_pkey" PRIMARY KEY ("id")
+);
