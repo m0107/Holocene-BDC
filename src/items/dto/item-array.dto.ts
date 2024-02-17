957e341901fb8c55f85dee20c6be78ff872212ea
mohit <mohit@mohits-MacBook-Pro.local>
2024-02-17 16:16:11 +0530

    bug fix

--- src/items/dto/item-array.dto.ts
+++ src/items/dto/item-array.dto.ts
@@ -6,8 +6,8 @@ import { ItemDto } from './item.dto';
 
 export class ItemArrayDto {
     @IsArray({ message: 'Items should be an array' })
-    @ArrayMinSize(1, { message: 'At least 10 items are required' })
-    @ValidateNested({ each: true, message: 'Hello item should be a valid ItemDto' })
+    @ArrayMinSize(1, { message: 'At least 1 items are required' })
+    @ValidateNested({ each: true, message: 'item should be a valid ItemDto' })
     @Type(() => ItemDto)
     items: ItemDto[];
 }
